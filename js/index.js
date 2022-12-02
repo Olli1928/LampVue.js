992a6ac29e225225d27597eafdba9683858b8ada
Oliver Olskær <oliv320b@easjdk.onmicrosoft.com>
2022-12-02 14:28:37 +0100

    Knapper kna ses

--- js/index.js
+++ js/index.js
@@ -19,20 +19,20 @@ Vue.createApp({
         cleanList() {
             this.Lamplist = [];
             this.error = null;
-            console.log("count cars : " + this.Lamplist.length);
+            console.log("count Lamps : " + this.Lamplist.length);
         },
         
-         getAllCars() {
+         getAlllamps() {
              //axios call that returns all the elements from the webservice
             axios.get(baseUri)
             .then(response => {
              var divtag = document.getElementById("content");
 
-             console.log("in function getAllCars");
+             console.log("in function getAllLamps");
              console.log("status code: "+ response.status );
 
-             //add the returning data from the webservice to the variable carlists
-             this.carslist = response.data;
+             //add the returning data from the webservice to the variable Lamplists
+             this.lampslist = response.data;
              this.status = response.status;
               
              console.log("length of the Lamplist array " + this.Lamplist.length)
@@ -67,7 +67,7 @@ Vue.createApp({
              console.log("length of the Lamplists array " + this.Lamplist.length)
             })
             .catch(error = (ex) => {
-              this.carslist = []
+              this.Lamplist = []
               this.error = ex.message
               console.log("Error:" + this.error);
             })      
