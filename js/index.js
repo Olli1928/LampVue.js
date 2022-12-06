011fe68956d678464ec5199bd2bd0ebe629c07c7
Cem Turan <cemx0121@edu.zealand.dk>
2022-12-06 14:36:05 +0100

    Implementeret 3.parts API kald

--- js/index.js
+++ js/index.js
@@ -1,79 +1,43 @@
-const baseUri = "https://lamprestapi.azurewebsites.net/api/Lamp"
+
+const lampeURL = "https://lamprestapi.azurewebsites.net/api/Lamp"
 Vue.createApp({
     data() {
         return {
-            nyliste:[],
-            Lamplist: [],
-            error: null,
-            statuscode:null,
-            getLampId:""
+           city: "",
+           country: "",
+           currentTime: "",
+           temp: 0,
+           sunrise: "",
+           sunset: "",
+           visibility: 0,
+           weatherDescription: "",
+           searchedCityName: "",
+           error: "",
+           sunriseImagePath: './Billeder/Sunrise.png'
         }
     },
-     created() {
-        // created() is a life cycle method, not an ordinary method
-        // created() is called automatically when the page is loaded
-        console.log("created method called")
-        this.getAlllamps()
-    },
     methods: {
-        cleanList() {
-            this.Lamplist = [];
-            this.error = null;
-            console.log("count Lamps : " + this.Lamplist.length);
-        },
+        GetWeatherInfo(searchedCityName){
+            console.log("In GetWeatherInfo method")
+            searchedURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCityName + "&appid=08ee16c2dc794824ee9b4d2f71a7091d"
+            console.log(searchedURL)
+            axios.get(searchedURL)
+            .then(response => {
+                console.log("Status kode:" + response.status);
+                this.city = response.data.name
+                this.country = response.data.sys.country
+                this.currentTime = new Date(response.data.dt * 1000).toTimeString().slice(0,15)
+                this.temp = Math.round(response.data.main.temp - 273.15)
+                this.sunrise = new Date(response.data.sys.sunrise * 1000).toTimeString().slice(0,5)
+                this.sunset = new Date(response.data.sys.sunset * 1000).toTimeString().slice(0,5)
+                this.visibility = response.data.visibility / 1000
+                this.weatherDescription = response.data.weather[0].description
+            })
+            .catch(error = (ex) => {
+                this.error = ex.message
+                console.log("Fejlkode:" + this.error)
+            })
+        }
         
-         getAlllamps() {
-             //axios call that returns all the elements from the webservice
-            axios.get(baseUri)
-            .then(response => {
-             var divtag = document.getElementById("content");
-
-             console.log("in function getAllLamps");
-             console.log("status code: "+ response.status );
-
-             //add the returning data from the webservice to the variable Lamplists
-             this.lampslist = response.data;
-             this.status = response.status;
-              
-             console.log("length of the Lamplist array " + this.Lamplist.length)
-
-
-            })
-            .catch(error = (ex) => {
-              //resultElement.innerHTML = generateErrorHTMLOutput(error);
-              this.Lamplist = []
-               this.error = ex.message
-              console.log("Error:" + this.error);
-            })      
-            
-        },
-        getByLampId(id){
-            //axios call that returns the items from a specified user 
-            uri = baseUri +/id/+id
-            axios.get(uri)
-            .then(response => {
-            
-            console.log("Uri: " + uri)
-
-             console.log("in function getByUserId");
-             console.log("status code: "+ response.status );
-
-             //add the returning data from the webservice to the variable posts
-            //  this.Lamplist = response.data;
-             this.Lamplist = [];
-             this.Lamplist.push(response.data);
-             this.status = response.status;
-              
-             console.log("length of the Lamplists array " + this.Lamplist.length)
-            })
-            .catch(error = (ex) => {
-              this.Lamplist = []
-              this.error = ex.message
-              console.log("Error:" + this.error);
-            })      
-        },
-           
-       
-       
-    }
+    }      
 }).mount("#app")
